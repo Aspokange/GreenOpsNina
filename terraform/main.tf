2eedb566bb3219f0662a9947ba9af1397bbeb686
Nina CHAUCHARD <nina.chauchard@protonmail.com>
2026-06-03 18:42:43 +0200

    Fix associate public ip adress

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "meditrack_ec2" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.meditrack_subnet.id
   vpc_security_group_ids      = [aws_security_group.meditrack_sg.id]
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   key_name                    = var.key_name
 
   tags = {
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "key_name" {
+  description = "Nom de la clé SSH AWS"
+  type        = string
+}
